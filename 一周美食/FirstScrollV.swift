aba979a6e3ed80a7d13b8a093a0d52e147b09f11
jgh33 <jgh@iMac.mshome.net>
2016-07-23 18:03:15 +0800

    添加ScrollView

---
+++ 一周美食/FirstScrollV.swift
@@ -0,0 +1,21 @@
+//
+//  FirstScrollV.swift
+//  一周美食
+//
+//  Created by jgh on 16/7/23.
+//  Copyright © 2016年 jgh. All rights reserved.
+//
+
+import UIKit
+
+class FirstScrollV: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- 一周美食/FirstVC.swift
+++ 一周美食/FirstVC.swift
@@ -15,10 +15,18 @@ class FirstVC: UIViewController, UIPopoverPresentationControllerDelegate {
     @IBOutlet weak var searchBtn: UIButton!
     @IBOutlet weak var scanBtn: UIButton!
     @IBOutlet weak var messageBtn: UIButton!
+    @IBOutlet weak var FirstScrollV: FirstScrollV!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setNavBar()
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        
+    }
+    func setNavBar()  {
         self.searchBtn.backgroundColor = UIColor.white()
         self.searchBtn.layer.cornerRadius = 12.0
         let width1 = self.cityBtn.frame.width
@@ -26,6 +34,8 @@ class FirstVC: UIViewController, UIPopoverPresentationControllerDelegate {
         let width3 = self.messageBtn.frame.width
         self.searchBtn.frame.size.width = UIScreen.main().bounds.size.width - width1 - width2 - width3 - 50
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -40,18 +50,17 @@ class FirstVC: UIViewController, UIPopoverPresentationControllerDelegate {
     @IBAction func scan(_ sender: AnyObject) {
         let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let pop = storyboard.instantiateViewController(withIdentifier: "ScanPopVC")
-        
-        
         pop.modalPresentationStyle = .popover
         pop.popoverPresentationController?.delegate = self
         pop.popoverPresentationController?.sourceView = sender as? UIView
         pop.popoverPresentationController?.sourceRect = sender.bounds
         pop.preferredContentSize = CGSize(width: 150, height: 84)
         pop.popoverPresentationController?.permittedArrowDirections = .up
-        
         self.present(pop, animated: true, completion: nil)
 
     }
+    
+    
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
